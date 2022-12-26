3ca9341d2b2d07b036abeec0a211b2d9d495b1f6
isha-paliwal3 <1032201235@mitwpu.edu.in>
2022-12-26 06:11:45 +0530

    Added movie trailer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navbar from './Navbar';
 function App() {
   return (
     <div className="App">
+      
       <Navbar/>
       <Banner />
       
--- src/Row.js
+++ src/Row.js
@@ -1,39 +1,78 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import './Row.css'
+import movieTrailer from 'movie-trailer';
+import YouTube from 'react-youtube'
+import { CloseOutlined } from '@mui/icons-material';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
+  // const [mName, setName] = useState([]);
+  // const [Desc, setDesc] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      // console.log(request);
       setMovies(request.data.results);
+      console.table(request.data.results);
       return request;
     }
     fetchData();
   }, [fetchUrl]);
 
-  // console.table(movies);
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    }
+    else {
+      movieTrailer(movie?.name || " ")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+        })
+        .catch((error) => console.log(error));
+        setName(movie.name);
+        setDesc(movie.overview);
+    }
+  }
+
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const opts = {
+    height: '300%',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  
 
   return (
-    <div className='row'>
-      <h2 className='row-title'>{title}</h2>
-      <div className='row-posters'>
-        {movies.map(movies => (
-          <img
-          key={movies.id}
-          className='row-poster'
-          src={base_url + movies.backdrop_path }
-          alt={movies.name} />
+    <>
+      <div className='row'>
+        <h2 className='row-title'>{title}</h2>
+        <div className='row-posters'>
+          {movies.map(movie => (
+            <img
+              key={movie.id}
+              onClick={() => handleClick(movie)}
+              className='row-poster'
+              src={base_url + movie.backdrop_path}
+              alt={movie.name} />
           ))}
 
+        </div>
+        {trailerUrl &&
+          <div className='trailer-container'>
+            <div className='trailer-info'>
+              <CloseOutlined className='close-btn' onClick={() => setTrailerUrl('')}/>
+              <YouTube videoId={trailerUrl} opts={opts} />
+            </div>
+          </div>
+        }
       </div>
-
-    </div>
+    </>
   )
 }
 
